f3b58290d4fa4c7dbdc7fc4a103791e92b19cd2d
Rubén Panadero <rpanadero@okode.com>
2020-04-21 13:14:42 +0200

    feat: updating plugin templates (androidx, swift 5, etc)

--- android/src/main/java/com/okode/firebase/Firebase.java
+++ android/src/main/java/com/okode/firebase/Firebase.java
@@ -4,8 +4,9 @@ import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
